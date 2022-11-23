243625d1b85b5683e1485c5902aee5f21e1ea078
sunkyeongW <bluelight0949@naver.com>
2022-11-24 00:05:17 +0900

    reviewed and studied the insufficient parts of the previous learning materials.

---
+++ code_06.py
@@ -0,0 +1,10 @@
+def solution(n=int):
+    for i in range(1,6):
+        for y in range(i):
+            print(i,end='')
+        print(end="\n")
+    
+        
+if __name__ == '__main__':
+
+    solution()
